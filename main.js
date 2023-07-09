32c003de7eef0321f9920183b7daf346d783d4f1
George <georgefbrockman@gmail.com>
2023-07-09 12:33:36 -0500

    adds init, clickHandler, and appendError functions

--- main.js
+++ main.js
@@ -4,8 +4,37 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+function init(){
+  
+  const hearts = document.querySelectorAll('span.like-glyph');
+  for (let heart of hearts){
+    heart.addEventListener('click', clickHandler);
+  }
+}
 
+function clickHandler(event){
+  
+  if(!event.target.classList.contains('activated-heart')){
+    mimicServerCall()
+    .then(() => {
+      event.target.textContent = FULL_HEART;
+      event.target.classList.add('activated-heart');
+    })
+    .catch(error => appendError(error))
+  }else{
+    event.target.textContent = EMPTY_HEART;
+    event.target.classList.remove('activated-heart');
+  }
+}
 
+function appendError(error){
+  document.querySelector('#modal').classList.remove('hidden');
+
+  document.querySelector('#modal-message').textContent = error;
+  setTimeout(() => document.querySelector('#modal').classList.add('hidden'), 3000);
+}
+
+init();
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
